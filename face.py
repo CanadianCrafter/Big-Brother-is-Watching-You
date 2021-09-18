39be36fa62556bee7dcc34814cc09f0cc7b3ea89
Tim Lu <timmylu2013@gmail.com>
2021-09-18 13:48:46 -0400

    teriteri colour, tuning, testing

--- face.py
+++ face.py
@@ -3,20 +3,28 @@ import cv2
 import dlib
 import math
 
+#----------------------------
+
+#hard coded
+eye_width = 80
+eye_height = 80
+
+#----------------------------
+
 cap = cv2.VideoCapture(0)
 vid_width = cap.get(3)
 vid_height = cap.get(4)
 
 detector = dlib.get_frontal_face_detector()
 
+path = '.\TERIble edit.png'  # the path of face image
+image = cv2.imread(path)
+img_width, img_height, channel = image.shape
 
 def move_eyeball(x_center, y_center, eye_pos):
-    return (eye_pos[0] + round((x_center/1034)*50) - 25, eye_pos[1] + round((y_center/979)*70) - 35)
+    return (eye_pos[0] + round((x_center/img_width)*eye_width) - round(eye_width/2), eye_pos[1] + round((y_center/img_height)*eye_height) - round(eye_height/2))
 
 while (True):
-    path = '.\TERIble edit.png'  # the path of face image (people.jpg)
-    image = cv2.imread(path, 0)
-
     # Capture frame-by-frame
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -24,10 +32,10 @@ while (True):
     faces = detector(gray)
     # print(faces)
     for face in faces:
-        x1 = round(1034 - (face.left()/vid_width)*1034)
-        y1 = round((face.top()/vid_height)*979)
-        x2 = round(1034 - (face.right()/vid_width)*1034)
-        y2 = round((face.bottom()/vid_height)*979)
+        x1 = round(img_width - (face.left()/vid_width)*img_width)
+        y1 = round((face.top()/vid_height)*img_height)
+        x2 = round(img_width - (face.right()/vid_width)*img_width)
+        y2 = round((face.bottom()/vid_height)*img_height)
 
         x_center = (x1 + x2) / 2
         y_center = (y1 + y2) / 2
@@ -36,9 +44,9 @@ while (True):
         # position of face
         cv2.rectangle(image, (x1,y1), (x2,y2),(0,255,0),3)
 
-        left_eye_pos = [350, 310]  # change the left_eye_pos to map the face position to eyeballs
+        left_eye_pos = [360, 310]  # change the left_eye_pos to map the face position to eyeballs
         # (220, 250) is the coordinate of left eye(left-top corner)
-        right_eye_pos = [515, 320]  # change the right_eye_pos to map the face position to eyeballs
+        right_eye_pos = [525, 320]  # change the right_eye_pos to map the face position to eyeballs
         # (320, 250) is the coordinate of right eye(left-top corner)
 
         left_eye_pos = move_eyeball(x_center, y_center, left_eye_pos)
@@ -51,6 +59,8 @@ while (True):
 
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
+
+    image = cv2.imread(path)
 
 # When everything done, release the capture
 cap.release()
