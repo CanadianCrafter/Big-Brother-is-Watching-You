c66a083f46ec0ab17bff0200831e14ed568181e7
Beihao Zhou <68083940+Rebecca-Chou@users.noreply.github.com>
2021-09-18 14:34:29 +0800

    Update face.py

--- face.py
+++ face.py
@@ -9,7 +9,9 @@ detector = dlib.get_frontal_face_detector()
 while (True):
     # Capture frame-by-frame
     ret, frame = cap.read()
+    path = r'C:\Users\....png'      #Edit the path of image
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    image = cv2.imread(path, 0)
 
     faces = detector(gray)
     # print(faces)
@@ -20,18 +22,18 @@ while (True):
         y2 = face.bottom()
         print(face)
         # position of face
-        cv2.rectangle(frame, (x1,y1), (x2,y2),(0,255,0),3)
-        # left_eye = x1+30
-        # right_eye = x2-30
+        cv2.rectangle(image, (x1,y1), (x2,y2),(0,255,0),3)
+
+
         # cv2.circle(frame, (left_eye, y1), 60, (0, 0, 0), -1)
         # cv2.circle(frame, (right_eye, y1), 60, (0, 0, 0), -1)
         # cv2.circle(frame, (left_eye, y1), 20, (255, 255, 255), -1)
         # cv2.circle(frame, (right_eye, y1), 20, (255, 255, 255), -1)
 
-    # Display the resulting frame
-    cv2.imshow('frame', frame) # May be replaced with a background image rather than what webcam catches
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
+        # Display the resulting frame
+        cv2.imshow('frame', frame) # May be replaced with a background image rather than what webcam catches
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            break
 
 # When everything done, release the capture
 cap.release()
